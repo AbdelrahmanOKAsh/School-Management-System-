4f83e47c776606949a8a88815a540fb9e1d16ce0
Abdelrahman Gamal <117834022+AbdelrahmanOKAsh@users.noreply.github.com>
2023-10-31 11:12:19 +0200

    Update STD_TYPES.h

--- STD_TYPES.h
+++ STD_TYPES.h
@@ -2,7 +2,7 @@
 *                           Author:  Abdelrahman Gamal Abdelwanies Mohamed                           *
 *			    Created: 16/10/2023                                                      *
 *			    Project: School Mangement System                                         *
-*			    File: STD_TYPES.h                                                             *
+*			    File: STD_TYPES.h                                                        *
 *****************************************************************************************************/
 #ifndef STD_TYPES_H_
 #define STD_TYPES_H_
